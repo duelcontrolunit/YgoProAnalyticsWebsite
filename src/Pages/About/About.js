d749b629fd9ea4509b6c42d61ec0d8dbbd4b1ee0
Erakedon <erakedon@gmail.com>
2019-03-10 16:52:29 +0100

    created basic folder structure

---
+++ src/Pages/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="AboutPage">
+                This is AboutPage
+            </div>
+         );
+    }
+}
+ 
+export default AboutPage;
---
+++ src/Pages/Main/MainPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="MainPage">
+                This is Main page
+            </div>
+         );
+    }
+}
+ 
+export default MainPage;
