73efec7fc719d0a220fc2cb230115189490faba6
Erakedon <erakedon@gmail.com>
2019-06-28 21:27:31 +0200

    added proper link to donate and discord buttons

--- src/Shared/DonateButton/DonateButton.js
+++ src/Shared/DonateButton/DonateButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DonateButton = () => {
     return ( 
-        <div className="DonateButton" href="http://lmgtfy.com/?q=Not_ready_yet!" target="_blank" rel="noopener noreferrer">
+        <div className="DonateButton" href="https://www.paypal.com/pools/c/89QaKlEzdr" target="_blank" rel="noopener noreferrer">
             <i className="fas fa-donate"></i>
             <div className="text">Donate</div>
         </div>
